bfa9206c4c0f2522447e0ef350f2c2496e68cdd5
you@example.com <you@example.com>
2017-08-06 15:12:50 +0800

    aaaa

--- js/main.js
+++ js/main.js
@@ -1,32 +1,56 @@
-var gameOperation = {
-  gameWidth:400,
-  gameHeight:400
+var gameOption = {
+    gameWidth: 400,
+    gameHeight: 400,
+    spritesheetSize: 50,
+    tileSize: 50,
+    fieldSize: 6,
+    tileTypes: 6,
+    offsetX: 50,
+    offsetY: 50,
+    tweenSpeed: 100,
+    fadeSpeed: 1000,
+    fallSpeed: 250
 };
-var game = new Phaser.Game(gameOperation.gameWidth, gameOperation.gameHeight, Phaser.AUTO, 'drag');
-var gameBoot = function() {
-    this.preload = function () {
-        console.log('preload boot');
-    }
+var NO_DRAG = 0;
+var HORIZONTAL_DRAG = 1;
+var VERTICAL_DRAG = 2;
+var GAME_STATE_IDLE = 0;
+var GAME_STATE_DRAG = 1;
+var GAME_STATE_STOP = 2;
 
-    this.create = function () {
-        game.state.start('preload');
-    }
+var game = new Phaser.Game(400,400,Phaser.AUTO,'dragColor',{
+    preload:preload,
+    create:create,
+    update:update
+});
+function preload() {
+    game.load.spritesheet('tiles', 'image/tiles.png', gameOption.spritesheetSize, gameOption.spritesheetSize);
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
 }
-var gamePreload = function() {
-    this.preload = function () {
-        game.load.spritesheet('tiles','image/tiles.png');
-    }
+function create() {
+    this.tileArray = [];
+    this.tilePool = [];
+    this.tileGroup = game.add.group();
+    this.tileGroup.x = gameOption.offsetX;
+    this.tileGroup.y = gameOption.offsetY;
+    this.tileMask = game.add.graphics(this.tileGroup.x,this.tileGroup.y);
+    this.tileMask.beginFill(0xffffff);
+    this.tileMask.drawRect(0,0,gameOption.fieldSize*gameOption.tileSize,gameOption.fieldSize*gameOption.tileSize);
+    this.tileGroup.mask = this.tileMask;
+    this.tileMask.visible = true;
+    for (var i = 0; i <gameOption.fieldSize;i++){
+        this.tileArray[i] = [];
+        for (var j = 0; j<gameOption.fieldSize;j++){
+            addTile(i,j);
+        }
 
-    this.create = function () {
-        game.state.start('create');
     }
 }
-var gameCreate = function() {
-    this.create = function () {
-        console.log('create create');
-    }
+function addTile(row, col) {
+
 }
-game.state.add('boot', gameBoot);
-game.state.add('create', gameCreate);
-game.state.add('preload', gamePreload);
-game.state.start('boot');
+function update() {
+
+}
